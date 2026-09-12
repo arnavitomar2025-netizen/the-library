e717b35c5e2d7038bb17dbd51d1ec066fa7a74ec
arnavitomar2025-netizen <arnavi.tomar2025@vitstudent.ac.in>
2026-09-12 22:12:07 +0530

    book hover

--- scriptt.js
+++ scriptt.js
@@ -183,7 +183,7 @@ signUpBtn.addEventListener(
 
 
         authMessage.textContent =
-            "Account created! Please check your Gmail and verify your email before continuing. 🤎";
+            "Account created!<br>Please check your Gmail and verify your email before continuing. 🤎";
 
         signUpBtn.disabled = true;
         loginBtn.disabled = true;
